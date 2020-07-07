d9c9c9ae9a2c27f246a4849423bda4b70510a60f
nmb <b.ninad50@gmail.com>
2020-07-08 01:07:45 +0530

    next chapter

--- official-doc-stuff/2_variables/src/main.rs
+++ official-doc-stuff/2_variables/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
+    //Below code will not execute because immutable
+    // let x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    //Try shadowing
     let x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
+    let x = x + 1; //Multiple declarations for same variable name
+    let x = x * 2; //Note that this will not give immutable error as it is being redeclared.
+
     println!("The value of x is: {}", x);
 }
