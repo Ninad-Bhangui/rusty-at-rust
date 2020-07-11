1685931b4d31a9dbd54958a1f1b73f0d5e1c5775
ninad.bhangui <ninad.bhangui@aurochssoftware.com>
2020-07-11 19:01:28 +0530

    temp code

--- official-doc-stuff/4_temperature_converter/src/main.rs
+++ official-doc-stuff/4_temperature_converter/src/main.rs
@@ -1,12 +1,11 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
 
     loop {
         println!("Type in the temperature in the format 10F/100C (Fahrehnheit/Celsius). Press q/Q to exit,");
 
-        let temp_string = {
+        let mut temp_string = {
             let mut input = String::new();
             io::stdin()
                 .read_line(&mut input)
@@ -15,13 +14,35 @@ fn main() {
             input.to_uppercase().trim().to_string()
         };
         if temp_string == "Q" {
-            println!("Exiting ..");
+            println!("Exiting..");
             break;
-        } else if temp_string.chars().last() != Some('C') && temp_string.chars().last() != Some('F')
-        {
-            println!("Could not determine if type was celsius or fahrehnheit");
-        } else {
-            println!("Entered temperature is : {}", temp_string);
         }
+
+        let scale = temp_string.split_off(temp_string.len() -1);
+        let temp_string : f32 = match temp_string.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid format!");
+                continue
+            }
+        };
+        if scale == "C" || scale == "F" {
+            let converted_temp = convert(temp_string, scale);
+
+            println!("Converted temperature is : {}", converted_temp);
+        } else {
+            println!("Could not determine Scale")
+        }
+    }
+}
+
+fn convert(temperature : f32, scale: String) -> f32{
+    if scale == "C"{
+        return temperature*9.0/5.0+32.0
+    } else if scale == "F" {
+        return (temperature-32.0)*5.0/9.0
+    } else {
+        
+        return temperature
     }
 }
