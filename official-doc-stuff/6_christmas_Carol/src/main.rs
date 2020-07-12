515780df5e3ceca10006aa0739a7c3fbcae7faee
nmb <b.ninad50@gmail.com>
2020-07-12 16:25:16 +0530

    christmas carol

--- official-doc-stuff/6_christmas_Carol/src/main.rs
+++ official-doc-stuff/6_christmas_Carol/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    let days = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eigth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+    let gifts = [
+        "a partridge in a pear tree",
+        "Two turtle doves",
+        "three french hens",
+        "four calling birds",
+        "five gold rings",
+        "six geese a-laying",
+        "seven swans a-swimming",
+        "eight maids a-milking",
+        "nine ladies dancing",
+        "ten lords a-leaping",
+        "eleven pipers piping",
+        "twelve drummers drumming",
+    ];
+    for (index, day) in days.iter().enumerate() {
+        println!("On the {} day of christmas my true love sent to me", day);
+        for i in (0..index + 1).rev() {
+            if i == 0 && index != 0 {
+                println!("and {}", gifts[i]);
+            } else {
+                println!("{}", gifts[i]);
+            }
+        }
+        println!("\n");
+    }
 }
