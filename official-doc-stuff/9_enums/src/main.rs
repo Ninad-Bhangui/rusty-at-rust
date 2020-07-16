bf0fdd6dfc065a82e2418ff81096156595e58c22
nmb <b.ninad50@gmail.com>
2020-07-16 23:52:04 +0530

    code

--- official-doc-stuff/9_enums/src/main.rs
+++ official-doc-stuff/9_enums/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("Hello, world!");
+
+    let skyrim = Game::rpg(String::from("Skyrim"));
+    let battlefield = Game::fps(String::from("Battlefield"));
+    let civilization = Game::turnbased(String::from("Battlefield"));
+}
+
+enum Game {
+    rpg(String),
+    fps(String),
+    turnbased(String),
+}
+
+impl Game {
+    fn describe(&self) {
+        println!("Name :");
+    }
+}
+
+fn game_length_predictor(game: Game) -> i32 {
+    match game {
+        rpg => 40,
+        fps => 8,
+        turnbased => 20,
+    }
 }
