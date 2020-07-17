3c29777b9f216fc84b63e064c51aa0a13631bc27
nmb <b.ninad50@gmail.com>
2020-07-17 09:21:39 +0530

    Enum Fixes #14

--- official-doc-stuff/9_enums/src/main.rs
+++ official-doc-stuff/9_enums/src/main.rs
@@ -1,27 +1,50 @@
 fn main() {
     println!("Hello, world!");
 
-    let skyrim = Game::rpg(String::from("Skyrim"));
-    let battlefield = Game::fps(String::from("Battlefield"));
-    let civilization = Game::turnbased(String::from("Battlefield"));
-}
+    let skyrim = Game::Rpg(String::from("Skyrim"));
+    let battlefield = Game::Fps(String::from("Battlefield"));
+    let civilization = Game::Turnbased(String::from("Sid Meir's Civilization 5"));
 
+    println!("{}", skyrim.describe());
+    println!(
+        "Length of {:?}! is predicted to be {}",
+        skyrim,
+        game_length_predictor(&skyrim)
+    );
+    println!("{}", battlefield.describe());
+    println!(
+        "Length of {:?}! is predicted to be {}",
+        battlefield,
+        game_length_predictor(&battlefield)
+    );
+    println!("{}", civilization.describe());
+    println!(
+        "Length of {:?}! is predicted to be {}",
+        civilization,
+        game_length_predictor(&civilization)
+    );
+}
+#[derive(Debug)]
 enum Game {
-    rpg(String),
-    fps(String),
-    turnbased(String),
+    Rpg(String),
+    Fps(String),
+    Turnbased(String),
 }
 
 impl Game {
-    fn describe(&self) {
-        println!("Name :");
+    fn describe(&self) -> String {
+        match self {
+            Game::Rpg(val) => format!("{} is a role playing game", val),
+            Game::Fps(val) => format!("{} is a first person shooter", val),
+            Game::Turnbased(val) => format!("{} is a turn based strategy game", val),
+        }
     }
 }
 
-fn game_length_predictor(game: Game) -> i32 {
+fn game_length_predictor(game: &Game) -> i32 {
     match game {
-        rpg => 40,
-        fps => 8,
-        turnbased => 20,
+        Game::Rpg(_val) => 40,
+        Game::Fps(_val) => 8,
+        Game::Turnbased(_val) => 20,
     }
 }
