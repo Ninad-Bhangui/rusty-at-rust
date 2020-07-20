1a4963e6190e8d11d1502b5a6992ebe113464917
nmb <b.ninad50@gmail.com>
2020-07-21 00:52:47 +0530

    Collections Fixes #16

--- official-doc-stuff/11_collections/src/main.rs
+++ official-doc-stuff/11_collections/src/main.rs
@@ -3,4 +3,20 @@ fn main() {
     let mut v = Vec::new();
     v.push(5);
     v.push(6);
+    // let third: &i32 = &v[2]; //Panics
+    match v.get(1) {
+        Some(third) => println!("The second element is {}", third),
+        None => println!("There is no second element."),
+    };
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    };
+    for i in &mut v {
+        *i += 50;
+    }
+    for i in &v {
+        println!("{}", i);
+    }
+    println!("{:?}", v)
 }
--- official-doc-stuff/11_collections/vector_extras.md
+++ official-doc-stuff/11_collections/vector_extras.md
@@ -1,1 +1,13 @@
-https://doc.rust-lang.org/std/collections/index.html
+<https://doc.rust-lang.org/std/collections/index.html>
+
+### Use a Vec when
+
+- Will only be appending to (or near) the end.
+- Stack
+- Heap allocated resizable Array
+
+### VecDeque
+
+- `Vec` supports insertion at both ends
+- queue
+- Double-ended Queue
