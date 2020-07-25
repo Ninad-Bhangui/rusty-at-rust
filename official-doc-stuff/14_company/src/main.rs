f59eb4d11fd6e62d580b98e1a908b2b86ced4da0
ninad.bhangui <ninad.bhangui@aurochssoftware.com>
2020-07-25 16:49:32 +0530

    rust practice

--- official-doc-stuff/14_company/src/main.rs
+++ official-doc-stuff/14_company/src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+use std::collections::HashMap;
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to company!");
+    let mut database: HashMap<String,String> = HashMap::new();
+    loop {
+        println!("Please select ");
+        println!("[1] Add employee to database");
+        println!("[2] Fetch employees in a department");
+        println!("[3] Fetch employees by department");
+        let mut text_interface_input = String::new();
+        io::stdin().read_line(&mut text_interface_input).expect("Failed to read line");
+
+        match text_interface_input.trim().parse() {
+            Ok(num) => {
+                match num {
+                    1 => add(& mut database),
+                    (_) => continue
+                }
+            },
+            Err(c) => {
+                println!("{} is an invalid input. Only (1-3) allowed",c);
+                continue
+            }
+        };
+    }
+}
+
+fn add(database: &mut HashMap<String,String>) {
+    let mut input = String::new();
+    println!("**************************Adding***************************");
+    println!("Enter an employee in the format : ");
+    println!("Add Name to Department");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+
 }
