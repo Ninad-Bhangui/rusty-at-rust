12e97254e5c3c01ed95d1962e11391cecf41ecef
nmb <b.ninad50@gmail.com>
2020-07-30 10:41:45 +0530

    generic code

--- official-doc-stuff/16_generics/src/main.rs
+++ official-doc-stuff/16_generics/src/main.rs
@@ -1,3 +1,29 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+impl Point<i32> {
+    fn distance_from_origin(&self) -> f32 {
+        ((self.x * self.x + self.y * self.y) as f32).sqrt()
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
+    println!("Distance from origin : {}", p.distance_from_origin());
+
+    let p1 = Point { x: 2.5, y: 3.5 };
+    println!("Distance from origin : {}", p1.distance_from_origin());
 }
